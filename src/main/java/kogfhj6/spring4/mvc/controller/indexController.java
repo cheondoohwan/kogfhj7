832c580aacf320330303330020c3e09e10ecb521
cheondoohwan <kogfhj6@naver.com>
2022-01-05 18:47:10 +0900

    변경

---
+++ src/main/java/kogfhj6/spring4/mvc/controller/indexController.java
@@ -0,0 +1,16 @@
+package kogfhj6.spring4.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+
+    @GetMapping("/index")
+    public String index(Model m){
+        m.addAttribute("msg", "Hello,World!!");
+
+        return "index";
+    }
+}
